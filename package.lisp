5cb0a02293eaaf66c7d46aabcdda2bc17463d9d1
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-10-09 18:07:25 -0700

    fixing/commenting package.lisp exports

--- package.lisp
+++ package.lisp
@@ -2,24 +2,31 @@
   (:use :cl)
   (:export #:*catch-application-errors*
            #:*default-event-handler*
+           ;; common conditions/accessors
            #:connection-info
            #:connection-error
            #:conn-errcode
            #:conn-errmsg
+           ;; common functions
            #:stats
            #:start-event-loop
            #:event-loop-exit
 
+           ;; timer functions
            #:delay
            #:timer
 
+           ;; signal handling functions
            #:signal-handler
            #:free-signal-handler
            #:clear-signal-handler
 
+           ;; dns conditions
            #:dns-error
+           ;; dns functions
            #:dns-lookup
 
+           ;; tcp conditions/accessors
            #:tcp-info
            #:tcp-socket
            #:tcp-error
@@ -27,6 +34,7 @@
            #:tcp-timeout
            #:tcp-refused
            #:socket-closed
+           ;; tcp functions
            #:close-socket
            #:close-tcp-server
            #:write-socket-data
@@ -36,14 +44,17 @@
            #:tcp-send
            #:tcp-server
 
+           ;; http conditions/accessors
            #:http-info
            #:http-error
            #:http-timeout
            #:http-refused
+           ;;http functions
            #:http-client
            #:http-response
            #:http-server
            #:close-http-server
+           ;; http classes/accessors
            #:http-request
            #:http-request-c
            #:http-request-method
@@ -51,7 +62,5 @@
            #:http-request-resource
            #:http-request-querystring
            #:http-request-headers
-           #:http-request-body
-
-           #:setup-signal-handler)
+           #:http-request-body)
   (:nicknames :as))
