092058664af148e952c8d6b10c52f99b5733947a
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-12-06 23:41:45 -0800

    forgot to close sevrer in stream test. fixed.

--- test/tcp-stream.lisp
+++ test/tcp-stream.lisp
@@ -12,26 +12,26 @@
                   (client-data nil))
         (test-timeout 3)
 
-        (as:tcp-server nil 31388
-          (lambda (sock data)
-            (setf server-data (babel:octets-to-string data))
-            ;; for good measure, test writing to a new stream from server
-            (write-sequence (babel:string-to-octets "don't say that")
-                            (make-instance 'as:async-io-stream :socket sock)))
-          nil)
-        
-        ;; launch a client, which will write its data to a stream
-        (as:delay
-          (lambda ()
-            (let ((stream (as:tcp-connect "127.0.0.1" 31388
-                            (lambda (sock stream)
-                              ;; got a response, read from the stream
-                              (let ((num-bytes (read-sequence *stream-buffer* stream)))
-                                (setf client-data (babel:octets-to-string (subseq *stream-buffer* 0 num-bytes)))
-                                (as:close-socket sock)))
-                            nil
-                            :stream t)))
-              (write-sequence (babel:string-to-octets "can i have your coat?") stream)))))
+        (let ((server (as:tcp-server nil 31388
+                        (lambda (sock data)
+                          (setf server-data (babel:octets-to-string data))
+                          ;; for good measure, test writing to a new stream from server
+                          (write-sequence (babel:string-to-octets "don't say that")
+                                          (make-instance 'as:async-io-stream :socket sock)))
+                        nil)))
+          ;; launch a client, which will write its data to a stream
+          (as:delay
+            (lambda ()
+              (let ((stream (as:tcp-connect "127.0.0.1" 31388
+                              (lambda (sock stream)
+                                ;; got a response, read from the stream
+                                (let ((num-bytes (read-sequence *stream-buffer* stream)))
+                                  (setf client-data (babel:octets-to-string (subseq *stream-buffer* 0 num-bytes)))
+                                  (as:close-socket sock)
+                                  (as:close-tcp-server server)))
+                              nil
+                              :stream t)))
+                (write-sequence (babel:string-to-octets "can i have your coat?") stream))))))
     (is (string= server-data "can i have your coat?"))
     (is (string= client-data "don't say that"))))
 
