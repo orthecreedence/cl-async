f4e573867e3c326b6f4e80b2d4363513783f6257
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-01-09 13:33:39 -0800

    updating alet macro so that a) it can be fired multiple times if its bound futures do, and b) it cannot run its body until ALL bindings have finished...this was the original intent, but it used a counter to track which futures had fired, which doesnt account for a future firing multiple times if its finished more than once (and preserves callbacks). alet still has a strange issue where if it binds to a future that fires multiple times, the body is run twice for each firing of that future. not really sure what the deal is

--- future.lisp
+++ future.lisp
@@ -188,13 +188,17 @@
          (finished-vals (gensym "finished-vals"))
          (finished-cb (gensym "finished-cb"))
          (args (gensym "args")))
-    `(let* ((,finished-future (make-future))
+    `(let* ((,finished-future (make-future :preserve-callbacks t))
             (,finished-vals nil)
             (,finished-cb
-              (let ((c 0))
-                (lambda ()
-                  (incf c)
-                  (when (<= ,num-bindings c)
+              ;; the hash table makes sure that *all* futures have fires at
+              ;; least once. used to be a simple counter, but that doesn't
+              ;; account for a future firiing multiple times before its alet
+              ;; brethren get a chance to finish.
+              (let ((track-future-fired (make-hash-table :test #'eq :size ,num-bindings)))
+                (lambda (future)
+                  (setf (gethash future track-future-fired) t)
+                  (when (<= ,num-bindings (hash-table-count track-future-fired))
                     (let ((vars (loop for bind in ',bind-vars collect (getf ,finished-vals bind))))
                       (apply #'finish (append (list ,finished-future) vars))))))))
        ;; for each binding, attach a callback to the future it generates that
@@ -211,7 +215,7 @@
               (attach (apply #'values future-gen)
                 (lambda (&rest ,args)
                   (setf (getf ,finished-vals ',bind) (car ,args))
-                  (funcall ,finished-cb)))))
+                  (funcall ,finished-cb (car future-gen))))))
        ;; return our future which gets fired when all bindings have completed.
        ;; gets events forwarded to it from the binding futures.
        (attach ,finished-future
