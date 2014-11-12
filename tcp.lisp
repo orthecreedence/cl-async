31613d6c2a344c6d6018281fd60b7a9c0e4145ca
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-11-11 21:40:38 -0800

    adding back in (commented out) code for buffering writes. i want to have it handy for easy testing

--- tcp.lisp
+++ tcp.lisp
@@ -202,9 +202,23 @@
                                      (babel:string-to-octets data :encoding :utf-8)
                                      data)))
                       (if (socket-connected socket)
-                          ;; call uv_write on our data
-                          (write-to-uvstream uvstream bytes)
-                          ;; the socket isn't connected yet. libuv is suppoers to
+                          ;; write the data directly to the stream
+                          (unless (socket-closed-p socket)
+                            (write-to-uvstream uvstream bytes))
+                          ;; buffer the socket data until the next event loop.
+                          ;; this avoids multiple (unneccesary) calls to uv_write,
+                          ;; which is fairly slow
+                          ;(progn
+                          ;  (write-to-buffer bytes (socket-buffer socket))
+                          ;  (unless (socket-buffering-p socket)
+                          ;    (setf (socket-buffering-p socket) t)
+                          ;    ;; flush the socket's buffer on the next loop
+                          ;    (as:with-delay ()
+                          ;      (unless (socket-closed-p socket)
+                          ;        (setf (socket-buffering-p socket) nil)
+                          ;        (write-to-uvstream uvstream (buffer-output (socket-buffer socket)))
+                          ;        (setf (socket-buffer socket) (make-buffer))))))
+                          ;; the socket isn't connected yet. libuv is supposed to
                           ;; queue the writes until it connects, but it doesn't
                           ;; actually work, so we do our own buffering here. this
                           ;; is all flushed out in the tcp-connect-cb.
