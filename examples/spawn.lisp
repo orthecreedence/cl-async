ad3c3c142b696917dafc35f16557a00ca589a0d5
Hraban Luyat <hraban@0brg.net>
2022-09-09 13:06:14 -0400

    docs: optimize spawn.lisp for PR comment

--- examples/spawn.lisp
+++ examples/spawn.lisp
@@ -1,8 +1,8 @@
-;;; Spawn 1000 subprocesses and wait for them all to wake. Implementation of
+;;; Spawn 100 subprocesses and wait for them all to wake. Implementation of
 ;;; sleep sort.
 
-(ql:quickload :cl-async)
-(ql:quickload :babel)
+(ql:quickload :cl-async :silent t)
+(ql:quickload :babel :silent t)
 
 (defun spawn-one (v)
   (let ((sh (format NIL "sleep ~f; echo ~:*~f~%" v)))
@@ -10,11 +10,9 @@
               :output
               (list :pipe
                     :read-cb
-                    ;; How do you reliably wait until this callback has been
-                    ;; called?
                     (lambda (pipe data)
                       (format T "~A~&" (babel:octets-to-string data)))))))
 
 (as:with-event-loop (:catch-app-errors t)
-  (let ((ar (loop for i upto 1000 collect (/ (random 100) 10))))
+  (let ((ar (loop for i below 100 collect (/ (random 100) 10))))
     (mapc #'spawn-one ar)))
